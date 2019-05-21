30a691d5cbe049f3e969b26bc088d7814bfb9705
Jeremy Brady <rhspunkerkid05@yahoo.com>
2019-05-21 09:14:15 -0700

    adding in authorization tokens and handlers

--- safe-space/src/components/Login.js
+++ safe-space/src/components/Login.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { Button, Form, FormGroup, Label, Input } from "reactstrap";
+import Loader from "react-loader-spinner";
+import { connect } from "react-redux";
 import styled from "styled-components";
 
 const StyledLogin = styled(Form)`
@@ -9,17 +11,52 @@ const StyledLogin = styled(Form)`
 `;
 
 export default class LoginForm extends React.Component {
+  state = {
+    credentials: {
+      username: "",
+      password: ""
+    }
+  };
+
+  handleChange = e => {
+    this.setState({
+      credentials: {
+        ...this.state.credentials,
+        [e.target.name]: e.target.value
+      }
+    });
+    };
+    
+    login = e => {
+        e.preventDefault();
+        this.props
+          .login(this.state.credentials)
+          .then(() => this.props.history.push('/protected'));
+      };
+
   render() {
     return (
       <StyledLogin>
         <FormGroup>
           <Label for="User">Username:</Label>
-          <Input type="user" name="user" id="login-user" />
+                <Input
+                    type="text"
+                    name="username"
+                    value={this.state.credentials.username}
+                    onChange={this.handleChange}
+                />
         </FormGroup>
         <FormGroup>
-          <Label for="Password">Password:</Label>
-          <Input type="password" name="password" id="Password" />
-        </FormGroup>
+          <Label for="password">Password:</Label>
+                <Input
+                    type="password"
+                    name="password"
+                    value={this.state.credentials.password}
+                    onChange={this.handleChange}
+                />
+            </FormGroup>
+            {this.props.error && <p className="error">{this.props.error}</p>}
+
         <Button color="primary">Submit</Button>
       </StyledLogin>
     );
